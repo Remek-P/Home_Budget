2e24d821d2260534cdc6236d537b69794cfac2b2
Remek-P <remek.piotrowski@wp.pl>
2023-04-11 21:18:59 +0200

    refactoring

--- home-budget/src/components/NewExpense/NewExpense.js
+++ home-budget/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from "react";
 import { useNavigate } from "react-router-dom"
-import {GlobalContext} from "../../context/GlobalStates";
-import {Snackbar} from "@mui/material";
+import { GlobalContext } from "../../context/GlobalStates";
+import { Snackbar } from "@mui/material";
 
 //TODO: styling
 
